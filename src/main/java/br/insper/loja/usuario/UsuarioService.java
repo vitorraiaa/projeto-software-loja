9adb817e5c969e2ab25b324ba76be5bfd4822fc8
Eduardo Santana <ezambomsantana@gmail.com>
2025-04-14 08:14:53 -0300

    add token in the resttemmplate calls

--- src/main/java/br/insper/loja/usuario/UsuarioService.java
+++ src/main/java/br/insper/loja/usuario/UsuarioService.java
@@ -1,5 +1,8 @@
 package br.insper.loja.usuario;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -9,14 +12,23 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class UsuarioService {
 
-    public Usuario getUsuario(String email) {
+    public Usuario getUsuario(String token, String email) {
         RestTemplate restTemplate = new RestTemplate();
 
+        String url = "http://usuario:8080/api/usuario/" + email;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token); // ou "Bearer " + token, se for o caso
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
         try {
-            return restTemplate
-                    .getForEntity("http://usuario:8080/api/usuario/" + email,
-                            Usuario.class)
-                    .getBody();
+            return restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    requestEntity,
+                    Usuario.class
+            ).getBody();
         } catch (HttpClientErrorException.NotFound e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
